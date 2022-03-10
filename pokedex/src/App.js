8316d456d6cd6127ac821dd2bdea927931205627
denisabadarau <denisa.ionela.badarau@gmail.com>
2022-03-10 12:17:40 -0800

    added search bar(search by name)

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,17 +2,32 @@ import './App.css';
 import Grid from '@material-ui/core/Grid';
 import Data from './data.json';
 import PokemonCard from './components/PokemonCard';
+import {useState} from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
     <div className="App">
       <div className="appContainer">
-        <div className='appHeader'>
+        <div className="appHeader">
           <h1>Pokedex</h1>
+          <input 
+            className="searchBar" 
+            placeholder="Search pokemon name..."
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
         </div>
-        <div className='pokedexContainer'>
+        <div className="pokedexContainer">
           <Grid container>
-          {Data.map((pokemon) => (
+          {Data.filter((val) => {
+            if(searchTerm == "") {
+              return val;
+            } else if(val.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+              return val;
+            }
+          }).map((pokemon) => (
             <Grid item key={pokemon.id} xs={12} md={6} lg={4}>
               <PokemonCard pokemon={pokemon} />
             </Grid>
